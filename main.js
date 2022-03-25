21b8e954e4c5a5f27ac8f90d1e392aea647cd3af
Junsang-mun <mun.js@aol.com>
2022-03-25 16:38:05 +0900

    updates

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/api', function(req,res) {
+    res.send('api');
+});
+
+app.post('/api/post', function(req, res){
+    res.send(req)
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
